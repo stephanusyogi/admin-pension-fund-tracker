ce4948b80eba32296a84724cd7350a8af9a31b0f
stephanus yogi <stephanusyogi12@gmail.com>
2023-03-16 02:30:16 +0700

    first commit

---
+++ application/views/v_dashboard.php
@@ -0,0 +1,20 @@
+
+<!-- BEGIN: Content -->
+<div class="content">
+    <div class="grid grid-cols-12 gap-6">
+        <div class="col-span-12 2xl:col-span-9">
+            <div class="grid grid-cols-12 gap-6">
+                <!-- BEGIN: General Report -->
+                <div class="col-span-12 mt-8">
+                    <div class="intro-y flex items-center h-10">
+                        <h2 class="text-lg font-medium truncate mr-5">
+                            General Report
+                        </h2>
+                    </div>
+                </div>
+                <!-- END: General Report -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END: Content -->
