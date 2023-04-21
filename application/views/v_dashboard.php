a335eb83af3d5db381c4a0c97fadf4f9b65e7ecd
stephanus yogi <stephanusyogi12@gmail.com>
2023-04-22 00:01:23 +0700

    progress

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -11,6 +11,52 @@
                             General Report
                         </h2>
                     </div>
+                    <div class="grid grid-cols-12 gap-6 mt-5">
+                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                            <div class="report-box zoom-in">
+                                <div class="box p-5">
+                                    <div class="flex">
+                                        <i data-lucide="user" class="report-box__icon text-primary"></i> 
+                                    </div>
+                                    <div class="text-3xl font-medium leading-8 mt-6">Soon</div>
+                                    <div class="text-base text-slate-500 mt-1">Total Users</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                            <div class="report-box zoom-in">
+                                <div class="box p-5">
+                                    <div class="flex">
+                                        <i data-lucide="user-cog" class="report-box__icon text-primary"></i> 
+                                    </div>
+                                    <div class="text-3xl font-medium leading-8 mt-6">Soon</div>
+                                    <div class="text-base text-slate-500 mt-1">Total Adminstrators</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                            <div class="report-box zoom-in">
+                                <div class="box p-5">
+                                    <div class="flex">
+                                        <i data-lucide="key" class="report-box__icon text-primary"></i> 
+                                    </div>
+                                    <div class="text-3xl font-medium leading-8 mt-6">Soon</div>
+                                    <div class="text-base text-slate-500 mt-1">Users Login This Week</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                            <div class="report-box zoom-in">
+                                <div class="box p-5">
+                                    <div class="flex">
+                                        <i data-lucide="activity" class="report-box__icon text-primary"></i> 
+                                    </div>
+                                    <div class="text-3xl font-medium leading-8 mt-6">Soon</div>
+                                    <div class="text-base text-slate-500 mt-1">Users Activity This Week</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <!-- END: General Report -->
             </div>
